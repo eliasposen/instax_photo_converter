09222d21ab204ce7e3627b1083ac02faf10d781c
esposen <ep2851@bard.edu>
2019-06-12 23:23:06 +0100

    added rotation

--- cropper.py
+++ cropper.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
+
 import pygame
+from pygame.locals import K_LEFT, K_RIGHT
 from PIL import Image
 from typing import Dict, Tuple
 
 
 class Cropper:
     def __init__(self, img_path: Path, out_img_path: Path, size: Tuple[int, int]):
-        self.img_path = img_path
         self.out_img_path = out_img_path
         self.size = size
         self.img = Image.open(img_path)
@@ -37,35 +38,36 @@ class Cropper:
         with TemporaryDirectory() as tmp_dir:
             tmp_img_path = Path(tmp_dir) / "tmp.jpg"
             self.resize(tmp_img_path)
-            pygame_img = pygame.image.load(str(tmp_img_path.absolute()))
-            surface = pygame.display.set_mode(pygame_img.get_rect()[2:])
-            n = True
-            while n:
-                surface.fill((225, 225, 225))
-                surface.blit(pygame_img, (0, 0))
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        n = False
-                        break
-                    pygame.display.update()
-        # coordinates = self.get_crop_cooridinates()
-        # self.crop_and_save(coordinates)
+            coordinates = self.get_crop_cooridinates(tmp_img_path)
+            self.crop_and_save(coordinates)
 
-    def setup(self):
+    def load_surface(self, img_path: Path) -> Tuple:
+        pygame_img = pygame.image.load(str(img_path.absolute()))
+        surface = pygame.display.set_mode(pygame_img.get_rect()[2:])
+        return surface, pygame_img
 
-        n = True
-        while n:
+    def get_crop_cooridinates(self, img_path: Path) -> Dict[str, int]:
+        surface, pygame_img = self.load_surface(img_path)
+        active_ui = True
+        while active_ui:
             surface.fill((225, 225, 225))
-            surface.blit(self.img, (0, 0))
+            surface.blit(pygame_img, (0, 0))
             for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == K_LEFT:
+                    self.img = self.img.transpose(Image.ROTATE_90)
+                    self.img.save(img_path)
+                    surface, pygame_img = self.load_surface(img_path)
+
+                if event.type == pygame.KEYDOWN and event.key == K_RIGHT:
+                    self.img = self.img.transpose(Image.ROTATE_270)
+                    self.img.save(img_path)
+                    surface, pygame_img = self.load_surface(img_path)
+
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    n = False
+                    active_ui = False
                     break
                 pygame.display.update()
-
-    def get_crop_cooridinates(self) -> Dict[str, int]:
+        pygame.quit()
         return {"left": 0, "top": 0, "right": 100, "bottom": 100}
 
     def crop_and_save(self, crop_cooridinates: Dict[str, int]) -> None:
@@ -76,6 +78,6 @@ if "__main__" == __name__:
     c = Cropper(
         Path("test_images/test_crop/crop_me.jpg"),
         Path("test_images/test_crop/cropped.jpg"),
-        (1000, 1000),
+        (1920, 1920),
     )
     c.launch()
